3a6f7a7389e8f40964f26a376915e08f965ef1f3
xelendt <m@xelendt.com>
2018-01-21 00:04:18 -0500

    web interface changes

--- index.js
+++ index.js
@@ -12,11 +12,39 @@ var cube = new THREE.Mesh( geometry, material );
 cube.matrixAutoUpdate  = false;
 cube.updateMatrix();
 
-camera.position.z = 5;
+camera.position.z = 10;
 
-var pointLight = new THREE.SpotLight( 0xffffff, 0.5 );
-pointLight.position.y = 10;
+var pointLight = new THREE.SpotLight( 0xffffff, 2.5 );
+pointLight.position.y = 25;
 
+pointLight.castShadow = true;
+pointLight.angle = 0.8;
+// pointLight.penumbra = 2;
+// pointLight.decay = 2;
+pointLight.distance = 100;
+// pointLight.shadow.mapSize.width = 256;
+// pointLight.shadow.mapSize.height = 256;
+
+pointLight.castShadow = true;
+
+var flashLight = new THREE.SpotLight( 0xffffff, 1.5 );
+flashLight.position = cube.position;
+flashLight.castShadow = true;
+flashLight.distance = 100;
+// pointLight.shadow.mapSize.width = 256;
+// pointLight.shadow.mapSize.height = 256;
+flashLight.rotation.x += Math.PI / 10;
+
+scene.add( pointLight );
+
+var geometry = new THREE.BoxGeometry( 40, 20, 20 );
+var material = new THREE.MeshPhongMaterial( {color: 0x555555, side:THREE.DoubleSide} );
+var plane = new THREE.Mesh( geometry, material );
+plane.receiveShadow = true;
+plane.position.y = 8;
+plane.rotation.x = Math.PI/2;
+
+scene.add( plane );
 
 var loader = new THREE.OBJLoader();
 
@@ -29,12 +57,20 @@ loader.load(
 		cube = object;
 		scene.add(object);
 		object.material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
+		object.castShadow = true;
+		object.receiveShadow = true;
 
 		object.traverse( function( child ) {
 			if( child instanceof THREE.Mesh ) {
-				child.material = new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: true } );
+				child.castShadow = true;
+				child.receiveShadow = true;
+				child.material = new THREE.MeshPhongMaterial( { color: 0xffffff } );
+
+				// child.material = new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: false } );
 			}
 		});
+
+		object.add(flashLight);
 		// object.position.z = ;
 		// console.log(scene);
 		// animate();
@@ -55,7 +91,7 @@ loader.load(
 	}
 );
 
-// renderer.render( scene, camera );
+renderer.render( scene, camera );
 
 // var t = 0;
 
@@ -70,12 +106,27 @@ function getPosFromPi(){
 				var parts = JSON.parse(this.responseText.replace(/\]\[/g,","));
 				var m = new THREE.Matrix4();
 
-				parts[3] /= 10;
-				parts[7] /= 10;
-				parts[11] /= 10;
+				// parts[3] /= 10;
+				// parts[7] /= 10;
+				// parts[11] /= 10;
 				// m.set( ...parts, 1, 1, 1, 1 );
-				cube.matrix.elements = [...parts, 0,0,0,1];
-				cube.matrix = cube.matrix.transpose();
+				// m.elements = [...parts, 0,0,0,1];
+
+				// cube.matrix.elements = [...parts, 0,0,0,1];
+				// cube.matrix = cube.matrix.transpose();
+				// cube.matrix.multiply(m);
+
+				//cube.rotation.y += 0.1;
+				cube.position.x = parts[3] / 100;
+				cube.position.y = parts[7] / 100;
+				cube.position.z = parts[11] / 100;
+
+				cube.rotation.x = 0;
+				cube.rotation.y = 0;
+
+				cube.rotation.y -= Math.PI / 2;
+				cube.rotation.x += Math.PI / 6;
+
 				// console.log(...parts, 1,1,1,1);
 				console.log(cube.matrix);
 				// cube.position.x = parts[3];
